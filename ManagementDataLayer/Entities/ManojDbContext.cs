ccdd1aac841fa29e817a852ddca30735c2393e51
manojkmj11 <mithuna692@gmail.com>
2023-04-08 12:39:22 +0530

    db changed

--- ManagementDataLayer/Entities/ManojDbContext.cs
+++ ManagementDataLayer/Entities/ManojDbContext.cs
@@ -21,7 +21,7 @@ public partial class ManojDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:manoj-kumar-server.database.windows.net,1433;Initial Catalog=ManojDB;Persist Security Info=False;User ID=Manoj;Password=anoj@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:teamperseverance.database.windows.net,1433;Initial Catalog=managementdb;Persist Security Info=False;User ID=manoj;Password=Team@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
